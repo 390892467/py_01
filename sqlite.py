00bed0598ae8e282e1260a65924687394a82a32c
zhenglm <zhenglm@163.com>
2016-07-13 00:22:21 +0800

    平安数据自动化导出

--- sqlite.py
+++ sqlite.py
@@ -6,9 +6,6 @@ cursor = conn.cursor()
 cursor.execute('select * from submitted where status=?',('1',))
 values = cursor.fetchall()
 filename = 'pa_' + time.strftime('%Y%m%d') + '.csv'
-filedir = str(r'c:\Users\zheng LM\Desktop\')
-fpath = os.path.join(filedir,filename)
-print(fpath)
 '''
 try:
     with open("fpath",'w') as file:
