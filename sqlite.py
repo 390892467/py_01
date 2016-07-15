0649b38380937f5f6eeb93b2887abb28ce2a4adc
zhenglm <zhenglm@qq.com>
2016-07-15 19:28:18 +0800

    2016-7-15提交

--- sqlite.py
+++ sqlite.py
@@ -1,16 +1,26 @@
+# coding: utf-8
 import sqlite3
 import time
 import os
-conn = sqlite3.connect(r'c:\Users\zheng LM\Desktop\sub.db')
+import sys
+
+if not os.path.isfile('/tmp/sub.db'):
+    sys.exit()
+filename = 'pa_' + time.strftime('%Y%m%d') + '.csv'
+if os.path.exists(filename):
+    os.remove(filename)
+
+conn = sqlite3.connect('/tmp/sub.db')
 cursor = conn.cursor()
 cursor.execute('select * from submitted where status=?',('1',))
 values = cursor.fetchall()
-filename = 'pa_' + time.strftime('%Y%m%d') + '.csv'
-'''
+#print(values)
 try:
-    with open("fpath",'w') as file:
+    with open("/tmp/{}".format(filename),'w',encoding='gbk') as file:
         for x in values:
             file.write(','.join(str(i)for i in list(x)) + '\n')
-finally:
-    os.path.join(fdir,filename)
-'''
+except Exception as E:
+    print(E)
+cursor.close()
+conn.commit()
+conn.close()
