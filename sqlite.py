bc9ad416747c968076e85d27b249356ac1925769
zhenglm <zhenglm@qq.com>
2016-07-18 11:59:26 +0800

    2016-7-18 completed！

--- sqlite.py
+++ sqlite.py
@@ -7,8 +7,6 @@ import sys
 if not os.path.isfile('/tmp/sub.db'):
     sys.exit()
 filename = 'pa_' + time.strftime('%Y%m%d') + '.csv'
-if os.path.exists(filename):
-    os.remove(filename)
 
 conn = sqlite3.connect('/tmp/sub.db')
 cursor = conn.cursor()
@@ -17,6 +15,7 @@ values = cursor.fetchall()
 #print(values)
 try:
     with open("/tmp/{}".format(filename),'w',encoding='gbk') as file:
+        file.write('id,name,phone,birthday,sex,give,dc1,dc2,status, ,response\n')
         for x in values:
             file.write(','.join(str(i)for i in list(x)) + '\n')
 except Exception as E:
@@ -24,3 +23,6 @@ except Exception as E:
 cursor.close()
 conn.commit()
 conn.close()
+
+
+
